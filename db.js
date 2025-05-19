cf901b874e9bbd3c0a6648d5dcbc64f93ce1332d
Joney <joneyabrahams@gmail.com>
2025-05-20 01:31:08 +0530

    17

--- db.js
+++ db.js
@@ -1,29 +1,9 @@
 const {MongoClient, ObjectId, ServerApiVersion} = require('mongodb')
 
-// let database
-
-// async function getDatabase(){
-//     const client = await MongoClient.connect('mongodb://127.0.0.1:27017')
-//     database = await client.db('TodoOwn')
-
-//     if(!database)
-//         console.log('Database not connected!')
-
-//     return database
-// }
-
-// module.exports = {
-//     getDatabase, ObjectId
-// }
-
-
-
-//second
-const uri = 'mongodb+srv://joney:1245@cluster0.5cxgcfn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 let database
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+const uri = "mongodb+srv://joney:1254@cluster0.59zlyxl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const client = new MongoClient(uri, {
-    tls: true, // ✅ force TLS
     serverApi: {
       version: ServerApiVersion.v1,
       strict: true,
@@ -31,20 +11,16 @@ const client = new MongoClient(uri, {
     }
   });
 
-  async function getDatabase() {
-    console.log('noob')
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      // Send a ping to confirm a successful connection
-      database = await client.db('TodoOwn')
-      console.log('✅ Connected to MongoDB Atlas')
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-      return database
-    }  catch (err) {
-        console.error('❌ MongoDB connection failed:', err);
-      }
-  }
+async function getDatabase(){
+    // const client = await MongoClient.connect('mongodb://127.0.0.1:27017')
+    await client.connect()
+    database = await client.db('TodoOwn')
+
+    if(!database)
+        console.log('Database not connected!')
+
+    return database
+}
 
 module.exports = {
     getDatabase, ObjectId
