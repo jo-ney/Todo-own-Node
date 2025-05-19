a152bcecb2f9f767a60d8d4243d282d8d6e6cd4e
Joney <joneyabrahams@gmail.com>
2025-05-19 22:53:49 +0530

    db change 7 gpt commit

--- db.js
+++ db.js
@@ -16,7 +16,7 @@ const {MongoClient, ObjectId, ServerApiVersion} = require('mongodb')
 //     getDatabase, ObjectId
 // }
 
-const uri = 'mongodb+srv://joney:1245@cluster0.5cxgcfn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const uri = 'mongodb+srv://joney:1245@cluster0.5cxgcfn.mongodb.net/TodoOwn?retryWrites=true&w=majority&appName=Cluster0'
 let database
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,7 +32,7 @@ const client = new MongoClient(uri, {
       // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
       // Send a ping to confirm a successful connection
-      database = await client.db("TodoOwn")
+      database = await client.db()
       console.log('✅ Connected to MongoDB Atlas')
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
       return database
