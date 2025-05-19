de78a2a5ad256b38186f757290ed0df67139420a
Joney <joneyabrahams@gmail.com>
2025-05-20 01:39:37 +0530

    18

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const {MongoClient, ObjectId, ServerApiVersion} = require('mongodb')
 
 let database
 
-const uri = "mongodb+srv://joney:1254@cluster0.59zlyxl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const uri = "mongodb+srv://joney:1254@cluster0.59zlyxl.mongodb.net/TodoOwn?retryWrites=true&w=majority&appName=Cluster0"
 const client = new MongoClient(uri, {
     serverApi: {
       version: ServerApiVersion.v1,
@@ -14,7 +14,7 @@ const client = new MongoClient(uri, {
 async function getDatabase(){
     // const client = await MongoClient.connect('mongodb://127.0.0.1:27017')
     await client.connect()
-    database = await client.db('TodoOwn')
+    database = await client.db()
 
     if(!database)
         console.log('Database not connected!')
